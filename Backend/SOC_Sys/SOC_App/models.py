e4f1a3a4b888ed3811ed2a31de70fd72d4060b91
Keith Cortes <11corteschristopher@gmail.com>
2026-06-06 15:57:31 +0800

    Updating Models

--- Backend/SOC_Sys/SOC_App/models.py
+++ Backend/SOC_Sys/SOC_App/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    ip_address = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, default="unknown")
+    is_active = models.BooleanField(default=True)
+
+class NetworkEvent(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    action = models.CharField(max_length=100)
+    service = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class SecurityIncident(models.Model):
+    incident_type = models.CharField(max_length=100)
+    severity = models.CharField(max_length=20)
+    ip_address = models.CharField(max_length=50)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
