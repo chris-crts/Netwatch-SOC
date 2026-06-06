7c92315d23f5cdeb5a97039a9d9fcb7cf235f978
Keith Cortes <11corteschristopher@gmail.com>
2026-06-06 15:57:47 +0800

    Updating VIEWS

--- Backend/SOC_Sys/SOC_App/views.py
+++ Backend/SOC_Sys/SOC_App/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Device, NetworkEvent, SecurityIncident
+from .serializers import DeviceSerializer, NetworkEventSerializer, SecurityIncidentSerializer
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class NetworkEventViewSet(viewsets.ModelViewSet):
+    queryset = NetworkEvent.objects.all()
+    serializer_class = NetworkEventSerializer
+
+class SecurityIncidentViewSet(viewsets.ModelViewSet):
+    queryset = SecurityIncident.objects.all()
+    serializer_class = SecurityIncidentSerializer
