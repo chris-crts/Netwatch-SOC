3f8aa0c1a86135bc639a006000ed24fc424a3c85
Keith Cortes <11corteschristopher@gmail.com>
2026-06-06 16:43:56 +0800

    Adding Consumers

---
+++ Backend/SOC_Sys/SOC_App/consumers.py
@@ -0,0 +1,14 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class SOCConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "WebSocket connected"}))
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.send(text_data=json.dumps({"echo": data}))
