b849af966c5571ed180c548ea9e9280727e1b61b
Keith Cortes <11corteschristopher@gmail.com>
2026-07-20 19:40:36 +0800

    Updating ASGI

--- Backend/SOC_Sys/SOC_Sys/asgi.py
+++ Backend/SOC_Sys/SOC_Sys/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import SOC_App.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SOC_Sys.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SOC_Sys.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            SOC_App.routing.websocket_urlpatterns
+        )
+    ),
+})
