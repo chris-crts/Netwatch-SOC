e91a45aa34e3d39a7916e62be97996ee990925f9
Keith Cortes <11corteschristopher@gmail.com>
2026-07-21 16:14:20 +0800

    Adding All the Updated and New Serializers Functions

--- Backend/SOC_Sys/SOC_App/serializers.py
+++ Backend/SOC_Sys/SOC_App/serializers.py
@@ -1,17 +1,34 @@
 from rest_framework import serializers
-from .models import Device, NetworkEvent, SecurityIncident
+from .models import Device, NetworkEvent, SecurityIncident, FirewallRule, TrafficLog
+
 
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
         fields = '__all__'
 
+
 class NetworkEventSerializer(serializers.ModelSerializer):
+    ip_address = serializers.CharField(source='device.ip_address', read_only=True)
+
     class Meta:
         model = NetworkEvent
-        fields = '__all__'
+        fields = ['id', 'device', 'ip_address', 'action', 'service', 'timestamp']
+
 
 class SecurityIncidentSerializer(serializers.ModelSerializer):
     class Meta:
         model = SecurityIncident
+        fields = '__all__'
+
+
+class FirewallRuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FirewallRule
+        fields = '__all__'
+
+
+class TrafficLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrafficLog
         fields = '__all__'
